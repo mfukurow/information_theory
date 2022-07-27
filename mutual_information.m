e7002ed7e3b789bff4c64bc531028cdb800adfd6
mfukurow <mfukurow@gmail.com>
2022-07-27 13:30:05 -0500

    modify mutual_information.m

--- mutual_information.m
+++ mutual_information.m
@@ -1,4 +1,4 @@
-function I = mutual_information(datamat, bincell)
+function I = mutual_information(v1, v2, bin1, bin2)
 
 %
 %   mutual_information.m
@@ -18,9 +18,20 @@ function I = mutual_information(datamat, bincell)
 %           I = mutual information
 %
 
-Hx = entropyH(multi_hist(datamat(:, 1), bincell(1)));
-Hy = entropyH(multi_hist(datamat(:, 2), bincell(2)));
-Hxy = entropyH(multi_hist(datamat, bincell));
+[v1r, v1c] = size(v1);
+[v2r, v2c] = size(v2);
+
+if v1r < v1c
+    v1 = v1';
+end
+
+if v2r < v2c
+    v2 = v2';
+end
+
+Hx = entropyH(v1, bin1);
+Hy = entropyH(v2, bin2);
+Hxy = entropyH([v1, v2], {bin1, bin2});
 I = Hx + Hy - Hxy;
 
 end
