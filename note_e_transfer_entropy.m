c053ff6c39eb48f1e0156d64dbd4a3f1145e98df
mfukurow <mfukurow@gmail.com>
2022-08-01 19:19:00 -0500

    check effective transfer entropy by simulation

---
+++ note_e_transfer_entropy.m
@@ -0,0 +1,20 @@
+n = 10000;
+n_rep = 20;
+qs = 2:2:20;
+d = 1;
+n_qs = length(qs);
+
+rlts = zeros(n_qs, 2);
+
+for ii = 1:n_qs
+    TEs = zeros(n_qs, 1);
+    for jj = 1:n_rep
+        a1 = randn(n, 1);
+        a2 = [randn(d, 1); a1((1:(end - d))) + randn(n - d, 1)];
+        qa1 = quantile(a1, nq);
+        qa2 = quantile(a2, nq);
+        TEs(jj) = e_transfer_entropy(a2, a1, qa2, qa1, d, 20);
+    end
+    rlts(ii, 1) = mean(TEs);
+    rlts(ii, 2) = std(TEs);
+end
